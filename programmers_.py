72e798684815fa84f58ee5c14688fb3e6bd03052
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-17 21:00:53 +0900

    programmers_숫자 문자열과 영단어

---
+++ programmers_.py
@@ -0,0 +1,19 @@
+#프로그래머스 숫자 문자열과 영단어
+
+def solution(s):
+    answer = s 
+    answer = answer.replace('zero', '0')
+    answer = answer.replace('one', '1')
+    answer = answer.replace('two', '2')
+    answer = answer.replace('three', '3')
+    answer = answer.replace('four', '4')
+    answer = answer.replace('five', '5')
+    answer = answer.replace('six', '6')
+    answer = answer.replace('seven', '7')
+    answer = answer.replace('eight', '8')
+    answer = answer.replace('nine', '9')
+
+
+    return int(answer)
+
+print(solution("one4seveneight"))
